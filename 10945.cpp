7621da9c689c7bd47dcab43fb5abdd4d878ce72f
Matej Kroflin <matej.kroflin@gmail.com>
2016-11-26 18:34:21 +0100

    MotherBear

--- 10945.cpp
+++ 10945.cpp
@@ -8,25 +8,18 @@ using namespace std;
 
 bool IsPalindrome(string s)
 {
-    int n = s.length();
-    for (int i = 0; i < n; ++i)
-        if (s[i] != s[n - i - 1])
-            return false;  
-                
-    return true;
+    return equal(s.begin(), s.begin() + s.size()/2, s.rbegin());
 }
 int main()
 {
     string s;
     while (getline(cin, s), s != "DONE")
     {
-        s.erase(remove_if(s.begin(), s.end(), (int(*)(int))isspace), s.end());
-        s.erase(remove_if(s.begin(), s.end(), (int(*)(int))ispunct), s.end());
+        s.erase(remove_if(s.begin(), s.end(), not1(ref(::isalpha))), s.end());
         transform(s.begin(), s.end(), s.begin(), ::toupper);
         if (IsPalindrome(s))
             cout << "You won't be eaten!\n";
         else
             cout << "Uh oh..\n";
     }
-    
 }
