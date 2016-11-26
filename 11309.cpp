8ed73b79428e621eeba17d8fd44a8b21ab900d40
Matej Kroflin <matej.kroflin@gmail.com>
2016-11-26 13:37:05 +0100

    CountingChaos

---
+++ 11309.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+bool IsPalindrome(string s)
+{
+    int n = s.length();
+    for (int i = 0; i < n; ++i)
+        if (s[i] != s[n - i - 1])
+            return false;  
+                
+    return true;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        int a;
+        cin >> s;
+        s.erase(s.begin() + 2);
+        do
+        {
+            a = stoi(s);
+            a++;
+            if (a % 100 > 59)
+                a = (a / 100 + 1) * 100;
+            if (a / 100 > 23)
+                a = 0;
+            s = to_string(a);
+        }while (!IsPalindrome(s));
+        while (s.length() < 4)
+            s.insert(s.begin(), '0');
+        
+        s.insert(s.begin() + 2, ':');    
+        cout << s << '\n';
+    }
+}
