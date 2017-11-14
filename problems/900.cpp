bfeb5ba5bab7136073470e869d80134b1c700900
Matej Kroflin <matej.kroflin@gmail.com>
2017-11-14 18:56:10 +0100

    BrickWallPatterns

---
+++ problems/900.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[51];
+
+int main()
+{
+    dp[1] = 1;
+    dp[2] = 2;
+    for (int i = 3; i < 51; ++i)
+        dp[i] = dp[i - 1] + dp[i - 2];
+
+    int n;
+    while (cin >> n, n)
+        cout << dp[n] << '\n';
+}
