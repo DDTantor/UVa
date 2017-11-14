28ccb9c6272dea37c4cd189e1604a70922dbb398
Matej Kroflin <matej.kroflin@gmail.com>
2017-11-14 11:53:54 +0100

    GoldbachsConjecture

--- problems/111.cpp
+++ problems/111.cpp
@@ -1,42 +1,38 @@
 #include <iostream>
-#include <cstring>
-#include <sstream>
+#include <vector>
 
 using namespace std;
 
-int dp[30][30], A[30], B[30];
+typedef long long ll;
+typedef vector<ll> vll;
 
-int lcs(int m, int n)
+vll Primes, IsPrime;
+
+void Sieve(ll bound)
 {
-    if (m == 0 || n == 0)
-        return 0;
+    IsPrime.assign(bound, 1);
+    IsPrime[0] = IsPrime[1] = 0;
 
-    if (dp[m][n] != -1)
-       return dp[m][n];
-    
-    if (A[m - 1] == B[n - 1])
-        return dp[m][n] = 1 + lcs(m - 1, n - 1);
-    else
-        return dp[m][n] = max(lcs(m, n - 1), lcs(m - 1, n));
+    for (ll i = 2; i < bound; ++i)
+    {
+        if (IsPrime[i])
+        {
+            for (ll j = i * i; j < bound; j += i)
+                IsPrime[j] = 0;
+
+            Primes.push_back(i);
+        }
+    }
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    int n, a;
-    cin >> n;
-    for (int i = 0; i < n; ++i)
-        cin >> a, A[a - 1] = i;
+    Sieve(1 << 15);
+    ll res[1 << 15] = {0}, n;
+    for (int i = 0; i < Primes.size(); ++i)
+        for (int j = i; j < Primes.size() && Primes[i] + Primes[j] <= (1 << 15); ++j)
+            ++res[Primes[i] + Primes[j]];
 
-    cin.ignore();
-    string s;
-    while (getline(cin, s))
-    {
-        istringstream iss(s);
-        for (int i = 0; i < n; ++i)
-            iss >> a, B[a - 1] = i;
-
-        memset(dp, -1, sizeof dp);
-        cout << lcs(n, n) << '\n';
-    }
+    while (cin >> n, n)
+        cout << res[n] << '\n';
 }
