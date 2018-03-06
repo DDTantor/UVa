dc4dfcb4cbc91a036f2d77d3f374e60edf8a8b24
Matej Kroflin <matej.kroflin@gmail.com>
2018-03-06 21:10:22 +0100

    MobilePhoneCoverage

--- problems/688.cpp
+++ problems/688.cpp
@@ -1,83 +1,42 @@
 #include <iostream>
 #include <iomanip>
-#include <set>
 #include <algorithm>
 
 using namespace std;
 
-struct vedge
-{
-    double x;
-    int id;
-    char c;
-    vedge() {}
-    bool operator <(const vedge& e) const {return x < e.x;}
-} VE[200];
+double x[100], y[100], r[100], dx[200], dy[200];
 
-struct hedge
+double inside(int k, int l, int n)
 {
-    double y;
-    int id;
-    char c;
-    hedge() {}
-    bool operator <(const hedge& e) const {return y < e.y;}
-} HE[200];
+    for (int i = 0; i < n; ++i)
+        if (x[i] - r[i] <= dx[k - 1] && dx[k] <= x[i] + r[i] &&
+            y[i] - r[i] <= dy[l - 1] && dy[l] <= y[i] + r[i])
+            return (dx[k] - dx[k - 1]) * (dy[l] - dy[l - 1]); 
 
-void S(double x, double y, double r, int i, int n)
-{
-    VE[i].id = VE[i + n].id = HE[i].id = HE[i + n].id = i;
-    VE[i + n].x = x + r;
-    VE[i].x = x - r;
-    HE[i + n].y = y + r;
-    HE[i].y = y - r;
-    VE[i].c = 'l', VE[i + n].c = 'r', HE[i].c = 'u', HE[i + n].c = 'd';
+    return 0;
 }
-
-void erase(set<hedge>& box, int id)
-{
-    for (auto it = box.begin(); it != box.end();)
-    {
-        if (it->id == id)
-            it = box.erase(it);
-        else
-            ++it;
-    }
-}
-           
 
 int main()
 {
-    int n, cs = 0;
+    int n, c = 1;
+
+    cout << fixed << setprecision(2);
     while (cin >> n, n)
     {
-        cout << ++cs << ' ';
-        double x, y, r;
         for (int i = 0; i < n; ++i)
-            cin >> x >> y >> r, S(x, y, r, i, n);
-
-        sort(VE, VE + 2 * n);
-        set<hedge> box;
-        double prevx, res = 0;
-        for (int i = 0; i < 2 * n; ++i)
         {
-            double x = VE[i].x, len = 0, prevy = box.begin()->y;
-            int cnt = 0;
-            for (hedge it : box)
-            {
-                double y = it.y;
-                len += (cnt && 1) * (y - prevy);
-                cnt += (it.c == 'u'? 1 : -1);
-                prevy = y;
-            }
-            
-            res += len * (x - prevx);
-            prevx = x;
-            if (VE[i].c == 'l')
-                box.insert(HE[VE[i].id]), box.insert(HE[VE[i].id + n]);
-            else
-                erase(box, VE[i].id);
+            cin >> x[i] >> y[i] >> r[i];
+            dx[i] = x[i] - r[i], dx[i + n] = x[i] + r[i];
+            dy[i] = y[i] - r[i], dy[i + n] = y[i] + r[i];
         }
+        
+        sort(dx, dx + 2 * n);
+        sort(dy, dy + 2 * n);
+        double res = 0;
+        for (int i = 1; i < 2 * n; ++i)
+            for (int j = 1; j < 2 * n; ++j)
+                res += inside(i, j, n);
 
-        cout << fixed << setprecision(2) << res << '\n';
+        cout << c++ << ' ' << res << '\n';
     }
 }
